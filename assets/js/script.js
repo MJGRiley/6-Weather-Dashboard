d9c003dbb100b5a14a4402cbb01ef2ea60bc7246
MJGR <mjgriley@gmail.com>
2021-09-28 21:11:57 -0400

    content working, css now

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,13 @@ var tArea = $('[type="text"]')
 tArea.submit(queryCity)
 var city
 var cityInfo
-var cityInfo2
 var today = moment().format('L')
 var lat
 var lon
 
 function queryCity() {
     city = tArea.val().trim()
+    localStorage.setItem("cityName",city)
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d5831cd7b791b23429c5fcd8777aab06&units=imperial"
     fetch(queryUrl,{
         cache: 'reload',
@@ -39,32 +39,37 @@ function queryTwo() {
         cityInfo = data
         console.log(data)
         displayWeather()
-        getFiveDay()
         localStorage.setItem("lastCity",JSON.stringify(data))
     })
 }
+$(document).ready(function () {
+    if (!JSON.parse(localStorage.getItem("lastCity"))){ 
+        return
+    } else { 
+        cityInfo = JSON.parse(localStorage.getItem("lastCity"))
+        city = localStorage.getItem('cityName')
+        displayWeather()
+    }
+})
 
 function addToList() {
     var templi = $('<li></li>')
     templi.text(city)
     var here = $('#addCityHere')
     here.append(templi)
-    console.log('WTF')
-
 }
 
 function displayWeather() {
     $('#cAndD').text(city + " (" + today + ")")
-    $('#icon')
+    $('#icon').attr('src'," http://openweathermap.org/img/wn/" + cityInfo.current.weather[0].icon + "@2x.png")
+    $('#icon').attr('alt',cityInfo.current.weather[0].description)
+    $('#icon').attr('title',cityInfo.current.weather[0].description)
     $('#temp').text("Temp: " + cityInfo.current.temp + "°F")
     $('#wind').text("Wind: " + cityInfo.current.wind_speed + " Mph")
     $('#hum').text("Humidity: " + cityInfo.current.humidity + "%")
     $('#uv').text("UVIndex: "+ cityInfo.current.uvi)
-}
-
-function getFiveDay() {
-
-}
-
-function storeLastCity() {
+    for(i=0;i<5;i++){
+        $('<li>',{class: "fiveDayLi"}).append(
+            $("<h4>"+cityInfo.daily[i].temp.day + "</h4><img class='icon' title:'" + cityInfo.daily[i].weather[0].description + "' alt:'" + cityInfo.daily[i].weather[0].description + "' src='http://openweathermap.org/img/wn/" + cityInfo.daily[i].weather[0].icon + "@2x.png'><p>" + cityInfo.daily[i].temp.day + "</p><p>" + cityInfo.daily[i].wind_speed+"</p><p>" + cityInfo.daily[i].humidity+"</p>")).appendTo($('#fiveDay'))
+    }
 }
