d88e762ecf5c1d21af9cb7270bf3f5bd3a6a1bc2
MJGR <mjgriley@gmail.com>
2021-09-28 23:40:19 -0400

    mostly working, previous searches broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ sBtn.click(queryCity)
 //not understanding why the click works and submit doesn't
 var tArea = $('[type="text"]')
 tArea.submit(queryCity)
+var preSearch = []
 var city
 var cityInfo
 var today = moment().format('L')
@@ -10,6 +11,7 @@ var lat
 var lon
 
 function queryCity() {
+    //if (!)
     city = tArea.val().trim()
     localStorage.setItem("cityName",city)
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d5831cd7b791b23429c5fcd8777aab06&units=imperial"
@@ -27,8 +29,8 @@ function queryCity() {
 }
 
 function queryTwo() {
-    queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityInfo.coord.lat + "&lon=" + cityInfo.coord.lon + "&appid=d5831cd7b791b23429c5fcd8777aab06&units=imperial&exclude=minutely,hourly,alerts"
     addToList()
+    queryUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityInfo.coord.lat + "&lon=" + cityInfo.coord.lon + "&appid=d5831cd7b791b23429c5fcd8777aab06&units=imperial&exclude=minutely,hourly,alerts"
     fetch(queryUrl,{
         cache: 'reload',
     })
@@ -37,7 +39,7 @@ function queryTwo() {
     })
     .then(function (data) {
         cityInfo = data
-        console.log(data)
+        $('#fiveDay').empty()
         displayWeather()
         localStorage.setItem("lastCity",JSON.stringify(data))
     })
@@ -53,23 +55,39 @@ $(document).ready(function () {
 })
 
 function addToList() {
+    if (city)
+    preSearch.push(city)
     var templi = $('<li></li>')
     templi.text(city)
-    var here = $('#addCityHere')
-    here.append(templi)
+    templi.attr('class','clickMe')
+    $('#addCityHere').append(templi)
+    $('.clickMe').click(listClick)
+
 }
 
 function displayWeather() {
     $('#cAndD').text(city + " (" + today + ")")
-    $('#icon').attr('src'," http://openweathermap.org/img/wn/" + cityInfo.current.weather[0].icon + "@2x.png")
-    $('#icon').attr('alt',cityInfo.current.weather[0].description)
-    $('#icon').attr('title',cityInfo.current.weather[0].description)
+    $('.icon').attr('src'," http://openweathermap.org/img/wn/" + cityInfo.current.weather[0].icon + "@2x.png")
+    $('.icon').attr('alt',cityInfo.current.weather[0].description)
+    $('.icon').attr('title',cityInfo.current.weather[0].description)
     $('#temp').text("Temp: " + cityInfo.current.temp + "°F")
     $('#wind').text("Wind: " + cityInfo.current.wind_speed + " Mph")
     $('#hum').text("Humidity: " + cityInfo.current.humidity + "%")
     $('#uv').text("UVIndex: "+ cityInfo.current.uvi)
+    console.log(cityInfo)
     for(i=0;i<5;i++){
         $('<li>',{class: "fiveDayLi"}).append(
-            $("<h4>"+cityInfo.daily[i].temp.day + "</h4><img class='icon' title:'" + cityInfo.daily[i].weather[0].description + "' alt:'" + cityInfo.daily[i].weather[0].description + "' src='http://openweathermap.org/img/wn/" + cityInfo.daily[i].weather[0].icon + "@2x.png'><p>" + cityInfo.daily[i].temp.day + "</p><p>" + cityInfo.daily[i].wind_speed+"</p><p>" + cityInfo.daily[i].humidity+"</p>")).appendTo($('#fiveDay'))
+            $("<h4>" + moment.unix(cityInfo.daily[i+1].dt).format('L') + 
+            "</h4><img class='icon' title:'" + cityInfo.daily[i].weather[0].description + 
+            "' alt:'" + cityInfo.daily[i].weather[0].description + "' src='http://openweathermap.org/img/wn/" + cityInfo.daily[i].weather[0].icon + 
+            "@2x.png'><p>Temp: " + cityInfo.daily[i].temp.day + "°F</p><p>Wind:" + 
+            cityInfo.daily[i].wind_speed+"Mph</p><p>Humidity: " + 
+            cityInfo.daily[i].humidity+"%</p>")).appendTo($('#fiveDay'))
     }
+}
+function listClick() {
+    console.log($(this).text())
+    tArea.val($(this).text())
+    queryCity()
+
 }
